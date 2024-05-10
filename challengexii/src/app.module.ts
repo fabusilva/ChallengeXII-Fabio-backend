b9cd5a437b6320d91a851c5d736e35ab7500eb4d
Fábio Silva <106494125+fabusilva@users.noreply.github.com>
2024-05-10 15:52:43 -0300

    fix:correcting errors in create users

--- challengexii/src/app.module.ts
+++ challengexii/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [UserModule],
-  controllers: [],
-  providers: [PrismaService],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
